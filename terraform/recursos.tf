bd34b713c43652f788c71c5da3f0df58e2e7d829
tiuf <52311699+tiuf@users.noreply.github.com>
2023-03-12 20:16:32 +0100

    Update recursos.tf

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -1,1 +1,154 @@
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location_name
+
+}
+
+#Creamos el container registry con el nombre casopractico2webserver sin el tag de geolocations dado que es una función del plan premium
+resource "azurerm_container_registry" "acr" {
+  name                = "casopractico2webserver"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true
+        }
+#Creamos el container registry con el nombre casopractico2appaks sin el tag de geolocations dado que es una función del plan premium
+resource "azurerm_container_registry" "acr1" {
+  name                = "casopractico2appaks"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true
+        }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vnic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+#IP PUBLICA
+resource "azurerm_public_ip" "pip" {
+  name                = "vip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+#VM1 Webserver
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  plan {
+    name      = "centos-8-stream-free"
+    product   = "centos-8-stream-free"
+    publisher = "cognosys"
+  }
+
+
+  source_image_reference {
+    publisher = "cognosys"
+    offer     = "centos-8-stream-free"
+    sku       = "centos-8-stream-free"
+    version   = "22.03.28"
+  }
+}
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "securitygroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "sshprule"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+ }
+
+resource "azurerm_subnet_network_security_group_association" "nsg-link" {
+   subnet_id                 = azurerm_subnet.subnet.id
+   network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_network_security_rule" "http" {
+  name                        = "http"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg1.name
+ }
+
+#AKS1
+resource "azurerm_kubernetes_cluster" "casopractico2_aks" {
+  name                = "aks1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
