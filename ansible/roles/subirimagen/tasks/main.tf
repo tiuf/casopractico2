71a77c7c0328228534be01f09c4617196feb32f7
tiuf <52311699+tiuf@users.noreply.github.com>
2023-03-17 20:24:42 +0100

    Update main.tf

--- ansible/roles/subirimagen/tasks/main.tf
+++ ansible/roles/subirimagen/tasks/main.tf
@@ -1,6 +1,8 @@
 ---
-# tasks file for subirimagen
+# La función de este rol es instalar todo los paquetes necesarios en la vm1, también un pull de nuestro registry de la imagen creada por nosotros y levanta el contenedor con podman.
 
+
+#En esta parte hace la instalación de los paquetes necesarios
 - name: Install a list of packages with a list variable
   ansible.builtin.yum:
     name: "{{ packages }}"
@@ -13,11 +15,12 @@
     - skopeo
     - openssl
 
-
+#En esta parte se hace el pull de la imagen del registry que hemos creado con terraform.
 - name: Pull an image
   containers.podman.podman_image:
     name: casopractico2webserver.azurecr.io/webserver:casopractico2
 
+#En esta parte se levanta un container con podman con la imagen que previamente hemos hecho el pull.
 - name: Start a container
   containers.podman.podman_container:
     name: webserver
